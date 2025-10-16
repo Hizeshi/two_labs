100abbc4dc90209f55d73a9c846929a798c2a9a1
Hizeshi <kschtirbu007@gmail.com>
2025-10-16 23:41:13 +0500

    Exercise 2: Create log.inc.php for visit tracking
    - Create timestamp variable
    - Capture current page from REQUEST_URI
    - Capture referrer from HTTP_REFERER
    - Format log entry with pipe separator
    - Write each visit as separate line to path.log
    - Store log file in /log/ directory

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,1 +1,12 @@
-<?
+<?php
+$dt = time();
+
+$page = $_SERVER['REQUEST_URI'];
+
+$ref = $_SERVER['HTTP_REFERER'] ?? '';
+
+$path = $dt . '|' . $page . '|' . $ref . "\n";
+
+$logFile = $_SERVER['DOCUMENT_ROOT'] . '/log/' . PATH_LOG;
+file_put_contents($logFile, $path, FILE_APPEND | LOCK_EX);
+?>
